5882b7cd2a69892d157d8d2dcdbb2928c59ac268
Felipe dos Santos <ofelipe.dev@gmail.com>
2021-11-28 01:24:18 -0300

    continuidade na criação

---
+++ app/swiper.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+
+    pagination: {
+        el: '.swiper-pagination'
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+
+})
